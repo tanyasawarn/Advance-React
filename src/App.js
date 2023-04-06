2df20fdd00c5211fb589e13a88114be3fb0e7270
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-06 12:24:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 import Expenses from "./Component/Expenses/Expenses";
+import NewExpense from "./Component/NewExpense/NewExpense";
 
-const App = ()=> {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 290,
-      date: new Date(2023, 4, 4),
-      location: "Super Mart",
-    },
-    {
-      id: "e2",
-      title: "TV",
-      amount: 29000,
-      date: new Date(2023, 7, 4),
-      location: "Electronics Store",
-    },
-    {
-      id: "e3",
-      title: "Groceries",
-      amount: 2900,
-      date: new Date(2023, 8, 4),
-      location: "Departmental Store",
-    },
-    {
-      id: "e4",
-      title: "Car Insurance",
-      amount: 2090,
-      date: new Date(2023, 9, 4),
-      location: "Showroom",
-    },
-  ];
+const Dummy_Expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 290,
+    date: new Date(2023, 4, 4),
+    location: "Super Mart",
+  },
+  {
+    id: "e2",
+    title: "TV",
+    amount: 29000,
+    date: new Date(2023, 7, 4),
+    location: "Electronics Store",
+  },
+  {
+    id: "e3",
+    title: "Groceries",
+    amount: 2900,
+    date: new Date(2023, 8, 4),
+    location: "Departmental Store",
+  },
+  {
+    id: "e4",
+    title: "Car Insurance",
+    amount: 2090,
+    date: new Date(2023, 9, 4),
+    location: "Showroom",
+  },
+];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(Dummy_Expenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
-      <h2>Let's get started!</h2>
-    <Expenses items={expenses} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
