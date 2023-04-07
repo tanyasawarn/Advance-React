73910442e0ef1d47a09968399884a3d3845c5142
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-07 19:15:24 +0530

    Add files via upload

---
+++ src/Component/Expenses/Expenses/ExpensesList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) =>{
+
+   
+     
+    if(props.items.length===0){
+        return (<h2 className='expenses-list__fallback'>Found No Expenses</h2>)
+    }
+
+    if(props.items.length===1){
+        return (
+            <ul className='expense-list'>
+        <h2 className='expenses-list__fallback'>Only single Expense here. Please add more...</h2>
+        {props.items.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+          </ul>
+        )
+    }
+   
+  return (
+    <ul className='expenses-list'>
+    {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      </ul>
+  )
+};
+
+export default ExpensesList;
